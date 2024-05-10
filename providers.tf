fc234cee65c8ccaaf850f079a3180cb31042ab5d
Chris ter Beke <1134120+ChrisTerBeke@users.noreply.github.com>
2024-05-10 17:41:36 +0200

    Add providers

---
+++ providers.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = "5.28.0"
+    }
+  }
+}
+
+provider "google" {
+  // always use resource-level configuration
+}
