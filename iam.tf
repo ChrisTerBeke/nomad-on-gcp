7bc3d0fc9ae45828c6d32fc55b7cbf1c471bf141
Chris ter Beke <1134120+ChrisTerBeke@users.noreply.github.com>
2024-05-13 16:03:37 +0200

    Fix IAM for nomad SA

--- iam.tf
+++ iam.tf
@@ -2,3 +2,10 @@ resource "google_service_account" "nomad" {
   account_id = "nomad-agent"
   project    = var.project
 }
+
+# Allow nomad to see other GCE instances
+resource "google_project_iam_member" "nomad_compute_viewer" {
+  project = var.project
+  role    = "roles/compute.viewer"
+  member  = "serviceAccount:${google_service_account.nomad.email}"
+}
