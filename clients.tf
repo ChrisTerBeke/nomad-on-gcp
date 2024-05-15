7674436c03c513caac2bfeed71b5f9c7b471358f
Chris ter Beke <1134120+ChrisTerBeke@users.noreply.github.com>
2024-05-15 18:32:29 +0200

    remove min_ready_sec

--- clients.tf
+++ clients.tf
@@ -62,7 +62,6 @@ resource "google_compute_region_instance_group_manager" "nomad_clients" {
     replacement_method           = "SUBSTITUTE"
     max_surge_fixed              = 3 # number of zones
     max_unavailable_fixed        = 0 # always create new clients before destroying old ones
-    min_ready_sec                = 120
   }
 
   auto_healing_policies {
